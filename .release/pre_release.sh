371307778070cc1ee6898065f2feaf2b047ad526
thek4n <thek4n@yandex.ru>
2024-12-10 15:16:28 +0300

    release scripts

--- .release/pre_release.sh
+++ .release/pre_release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sed "s/%VERSION%/v$(cat VERSION)/g" .README.md.template > README.md
+sed "s/%VERSION%/v$(cat VERSION)/g" ./.release/README.md.template > README.md
 git tag -s -a "v$(cat VERSION)" -m "v$(cat VERSION)"
