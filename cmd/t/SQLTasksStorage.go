7d4cabdaab8b89c06d41890e14d8dabc2a7d6be5
thek4n <thek4n@yandex.ru>
2024-12-23 13:38:37 +0300

    style

--- cmd/t/SQLTasksStorage.go
+++ cmd/t/SQLTasksStorage.go
@@ -17,7 +17,14 @@ func initTaskStorage(namespace string) storage.TasksStorage {
 	}
 	defer db.Close()
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS tasks(name varchar(90) primary key, namespace varchar(30) not null, born timestamp default CURRENT_TIMESTAMP not null, content text);")
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS
+	tasks(
+		name varchar(90) primary key,
+		namespace varchar(30) not null,
+		born timestamp default CURRENT_TIMESTAMP not null,
+		content text not null);
+	`)
 
 	return &storage.SqlTasksStorage{DbPath: DB_PATH}
 }
--- init.sh
+++
@@ -1,1 +0,0 @@
-sqlite3 test.sqlite3 'CREATE TABLE tasks(name varchar(90) primary key, namespace varchar(30) not null, born timestamp default CURRENT_TIMESTAMP not null, content text);'
