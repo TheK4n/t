0316080efaeb906376151c315fe5d4f382f04c72
thek4n <thek4n@yandex.ru>
2025-04-07 15:37:07 +0300

    add building with sqlite on android

--- .release/gen_packages.sh
+++ .release/gen_packages.sh
@@ -15,6 +15,16 @@ build_sqlite() {
     GOOS=linux GOARCH="${arch}" go build --tags tsqlite -ldflags "-w -s" -o ./t ./cmd/t
 }
 
+build_sqlite_android_arm64() {
+    go generate ./...
+    go fmt ./...
+    CGO_ENABLED=1 \
+    GOOS="android" \
+    GOARCH="arm64" \
+    CC="aarch64-linux-android21-clang" \
+    go build --tags tsqlite -ldflags "-w -s" -o ./t ./cmd/t
+}
+
 pack() {
     arch="${1}"
     tar czf "t_v$(cat VERSION)_linux_${arch}.tar.gz" ./t
@@ -37,7 +47,14 @@ build_and_pack_sqlite() {
     pack_sqlite "${arch}"
 }
 
+build_and_pack_sqlite_android() {
+    build_sqlite_android_arm64
+    tar czf "t_v$(cat VERSION)-sqlite_android_arm64.tar.gz" ./t
+}
+
 build_and_pack_sqlite "amd64"
 
 build_and_pack "arm64"
 build_and_pack "amd64"
+
+build_and_pack_sqlite_android
