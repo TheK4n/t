82ddbbee6ee2ec6905153d1434961a6e078b28a9
thek4n <thek4n@yandex.ru>
2025-05-23 12:11:19 +0300

    remove fs type

--- gen_packages.sh
+++ gen_packages.sh
@@ -8,31 +8,19 @@ build() {
     GOOS=linux GOARCH="${arch}" go build -ldflags "-w -s" -o ./t ./cmd/t
 }
 
-build_sqlite() {
-    arch="${1}"
-    go generate ./...
-    go fmt ./...
-    GOOS=linux GOARCH="${arch}" go build --tags tsqlite -ldflags "-w -s" -o ./t ./cmd/t
-}
-
-build_sqlite_android_arm64() {
+build_android_arm64() {
     go generate ./...
     go fmt ./...
     CGO_ENABLED=1 \
     GOOS="android" \
     GOARCH="arm64" \
     CC="aarch64-linux-android21-clang" \
-    go build --tags tsqlite -ldflags "-w -s" -o ./t ./cmd/t
+    go build -ldflags "-w -s" -o ./t ./cmd/t
 }
 
 pack() {
     arch="${1}"
     tar czf "t_v$(cat VERSION)_linux_${arch}.tar.gz" ./t
-}
-
-pack_sqlite() {
-    arch="${1}"
-    tar czf "t_v$(cat VERSION)-sqlite_linux_${arch}.tar.gz" ./t
 }
 
 build_and_pack() {
@@ -41,20 +29,12 @@ build_and_pack() {
     pack "${arch}"
 }
 
-build_and_pack_sqlite() {
-    arch="${1}"
-    build_sqlite "${arch}"
-    pack_sqlite "${arch}"
+build_and_pack_android() {
+    build_android_arm64
+    tar czf "t_v$(cat VERSION)-android_arm64.tar.gz" ./t
 }
-
-build_and_pack_sqlite_android() {
-    build_sqlite_android_arm64
-    tar czf "t_v$(cat VERSION)-sqlite_android_arm64.tar.gz" ./t
-}
-
-build_and_pack_sqlite "amd64"
 
 build_and_pack "arm64"
 build_and_pack "amd64"
 
-build_and_pack_sqlite_android
+build_and_pack_android
